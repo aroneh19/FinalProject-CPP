b5bb8d43eabd8618aca622565cdd00518ebcd379
Aron Elí Herreros <aroneh19@gmail.com>
2025-04-19 17:04:15 +0000

    took out icons

--- include/game/Team.h
+++ include/game/Team.h
@@ -3,14 +3,15 @@
 #include <characters/Character.h>
 #include <items/Item.h>
 
+class Team
+{
+public:
+    void addCharacter(Character character);
+    void addItem(Item item);
+    const std::vector<Character> &getCharacters() const;
+    const std::vector<Item> &getItems() const;
 
-class Team {
-    public:
-        void addCharacter(Character character);
-        void addItem(Item item);
-        const std::vector<Character>& getCharacters() const;
-        const std::vector<Item>& getItems() const;
-    private:
-        std::vector<Character> team;
-        std::vector<Item> items;
+private:
+    std::vector<Character> team;
+    std::vector<Item> items;
 };
